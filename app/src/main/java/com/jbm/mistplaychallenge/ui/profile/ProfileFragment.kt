9bb2118c8455f03c2996a6f67dc27147c9229b6e
Jean-Baptiste Meyer <meyer.jeanbaptiste@gmail.com>
2021-01-18 19:36:59 -0500

    First iteration of adding view dynamically into an other view

--- app/src/main/java/com/jbm/mistplaychallenge/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/jbm/mistplaychallenge/ui/profile/ProfileFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.ScrollView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -22,10 +24,16 @@ class ProfileFragment : Fragment() {
         profileViewModel =
                 ViewModelProvider(this).get(ProfileViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_profile, container, false)
-        val textView: TextView = root.findViewById(R.id.text_profile)
-        profileViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+        //building the horizontal scrolling list of game
+        val ll : LinearLayout = root.findViewById(R.id.game_card_area)
+
+        for (i in 1..10) {
+            val gameCardLayout: View = layoutInflater.inflate(R.layout.game_card, ll, false)
+            gameCardLayout.findViewById<TextView>(R.id.game_card_title).text = "coucou"
+            ll.addView(gameCardLayout)
+        }
+
         return root
     }
 }
